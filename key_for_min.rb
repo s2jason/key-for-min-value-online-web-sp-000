962a307b4ebe41747534bb241190c09d67faa059
Jason Keeling <s2jason@yahoo.com>
2020-08-14 22:32:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,12 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.each do |k,v|
+    if min_value == nil
+      min_key = k
+      min_value = v
+    elsif min_value >
     
+      
   
     
 end
